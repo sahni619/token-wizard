6cd373f9a14d5227a7e2c0c377a19684290934f5
Mariano Aguero <mariano.aguero@altoros.com>
2018-08-03 10:34:23 -0300

    Add reset function

--- src/stores/DeploymentStore.js
+++ src/stores/DeploymentStore.js
@@ -12,6 +12,7 @@ class DeploymentStore {
   @observable invalidAccount = false
 
   constructor() {
+    this.reset()
     autosave(this, 'DeploymentStore', store => {
       const txMap = new Map()
       Object.keys(store.txMap).forEach(key => {
@@ -151,6 +152,15 @@ class DeploymentStore {
   @computed
   get deployInProgress() {
     return this.deploymentStep !== null
+  }
+
+  @action
+  reset = () => {
+    this.txMap = new Map()
+    this.deploymentStep = null
+    this.hasEnded = false
+    this.deployerAccount = null
+    this.invalidAccount = false
   }
 }
 
